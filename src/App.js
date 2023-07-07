db07f35e8abbaf8a6ca489e66e29be180457466b
rttoshio <rttoshio@hotmail.com>
2023-07-07 11:47:44 -0300

    task 4 done. OutputResults Conditionally

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import UserInput from "./Components/UserInput/UserInput";
 function App() {
   const [userInput, setUserInput] = useState(null);
   const calculateHandler = (userInput) => {
-    setUserInput();
+    setUserInput(userInput);
   };
   const yearlyData = [];
 
   if (userInput) {
-    let currentSavings = +userInput["current-savings"];
-    const yearlyContribution = +userInput["yearly-contribution"];
-    const expectedReturn = +userInput["expected-return"] / 100;
-    const duration = +userInput["duration"];
+    let currentSavings = userInput["current-savings"];
+    const yearlyContribution = userInput["yearly-contribution"];
+    const expectedReturn = userInput["expected-return"] / 100;
+    const duration = userInput["duration"];
 
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
@@ -33,8 +33,13 @@ function App() {
       <Header />
 
       <UserInput onCalculate={calculateHandler} />
-
-      <ResultsTable />
+      {!userInput && <p> No investment calculated yet. </p>}
+      {userInput && (
+        <ResultsTable
+          data={yearlyData}
+          initialInvestment={userInput["current-savings"]}
+        />
+      )}
     </div>
   );
 }
--- src/Components/ResultsTable/ResultsTable.js
+++ src/Components/ResultsTable/ResultsTable.js
@@ -1,4 +1,11 @@
-const ResultsTable = () => {
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const ResultsTable = (props) => {
   return (
     /* Todo: Show below table conditionally (only once result data is available) */
     /* Show fallback text if no data is available */
@@ -13,6 +20,26 @@ const ResultsTable = () => {
         </tr>
       </thead>
       <tbody>
+        {props.data.map((yearData) => (
+          <tr key={yearData.year}>
+            <td>{yearData.year}</td>
+            <td>{formatter.format(yearData.savingsEndOfYear)}</td>
+            <td>{formatter.format(yearData.yearlyInterest)}</td>
+            <td>
+              {formatter.format(
+                yearData.savingsEndOfYear -
+                  props.initialInvestment -
+                  yearData.yearlyContribution * yearData.year
+              )}
+            </td>
+            <td>
+              {formatter.format(
+                props.initialInvestment +
+                  yearData.yearlyContribution * yearData.year
+              )}
+            </td>
+          </tr>
+        ))}
         <tr>
           <td>YEAR NUMBER</td>
           <td>TOTAL SAVINGS END OF YEAR</td>
